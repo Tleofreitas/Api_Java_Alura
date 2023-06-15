9cdf7f8d3693efe19100c8990412b356fe0c502d
Thiago <thiagoleonardo1995@live.com>
2023-06-15 16:59:51 -0300

    Alteração de Médicos Ok

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedicos;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,5 +30,13 @@ public class MedicoController {
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) { // Pageable = Paginação
         // Stream e Map para converter lista de Medico para lista de DadosListagemMedico
         return repository.findAll(pageable).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping // Post chama o atualizar() - PUT = Atualizar dados
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedicos dados) {
+        //Trazer os dados atuais do médico pelo Id
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 }
