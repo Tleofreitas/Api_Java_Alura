8e905e28c4be4b2c97fc4a2378eb9f43e4329537
Thiago <thiagoleonardo1995@live.com>
2023-06-16 18:02:18 -0300

    Ajustado retorno das requisições de validação

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,15 +2,36 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // Classe para isolar o tratamento de erros da API
 @RestControllerAdvice
 public class TratadorDeErros {
+
     // Capturar o erro 500 e devolver 404
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    // Capturar o erro 400 (validações) e devolver personalizado
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        // Capturar as Exceções lançadas
+        var erros = ex.getFieldErrors();
+
+        // Converter a lista inteira do retorno do erro para o DTO
+        // com as informações que queremos
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    // DTO internamente pois só será usado aqui
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
