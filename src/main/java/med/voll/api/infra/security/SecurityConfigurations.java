da789fc570dbdb99b23ea309c7499717b77b3543
Thiago <thiagoleonardo1995@live.com>
2023-06-19 10:37:59 -0300

    Método de segurança alterado para Stateless

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,22 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Configurações de Segurança
+@Configuration
+@EnableWebSecurity // Configurações personalizadas
+public class SecurityConfigurations {
+
+    // Processo de Autenticação Stateless
+    @Bean // Devolver o Objeto
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
