6314e4c0bc27b61b2e198c69f4fdffaee142b79c
Thiago <thiagoleonardo1995@live.com>
2023-06-21 16:20:39 -0300

    Gerando tokens JWT

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    // Geração do TOKEN
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Voll.med") // Quem está gerando o TOKEN
+                    .withSubject(usuario.getLogin()) // Qual o usuário
+                    .withExpiresAt(dataExpiracao()) // Validade do TOKEN
+                    .sign(algoritmo);
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Erro ao gerar TOKEN JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        // Validade de 2 horas
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
