94267a388d5a0b29d8a6a13814d9601a27d7b70f
Thiago <thiagoleonardo1995@live.com>
2023-06-27 17:56:43 -0300

    Agendar consulta iniciado

---
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public class DadosDetalhamentoConsulta {
+    public DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+    }
+}
