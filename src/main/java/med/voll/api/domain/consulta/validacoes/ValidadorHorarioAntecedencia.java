e52543eb63939c8bce8f08edff346be5091b89a7
Thiago <thiagoleonardo1995@live.com>
2023-06-28 16:11:05 -0300

    Classes de Validação

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioAntecedencia {
+    public void validar(DadosAgendamentoConsulta dados) {
+        // Pegar o horário de agendamento
+        var dataConsulta = dados.data();
+        // Pegar a data do momento
+        var agora = LocalDateTime.now();
+        // Verificar se esta dentro da regra de 30 min de antecedência
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < 30) {
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+        }
+    }
+}
