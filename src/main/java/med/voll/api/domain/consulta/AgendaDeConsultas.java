a6fac179b20db2bc6b49feecbf75c53a659cb016
Thiago <thiagoleonardo1995@live.com>
2023-06-28 09:56:06 -0300

    Validações de integridade

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,26 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+        // Checar se Id do Paciente existe
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        // Checar se o Id do Médico não é nulo, pois é opcional
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
         // Carregar o paciente pelo id
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         // Carregar o medico pelo id
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         // Gerar a Consulta
         var consulta = new Consulta(null, medico, paciente, dados.data());
         // Salvar a consulta no banco de dados
         consultaRepository.save(consulta);
+    }
+
+    private void escolherMedico(DadosAgendamentoConsulta dados) {
     }
 }
