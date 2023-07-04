a229e7358a0cb73ba6755d25ac15cca4a775556f
Thiago <thiagoleonardo1995@live.com>
2023-07-04 14:11:17 -0300

    Configurando o banco de Testes Automatizados

--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -2,10 +2,7 @@ package med.voll.api.domain.consulta;
 
 import java.time.LocalDateTime;
 
-public class DadosDetalhamentoConsulta {
-    public DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
-    }
-
+public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
     public DadosDetalhamentoConsulta(Consulta consulta) {
         this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
     }
